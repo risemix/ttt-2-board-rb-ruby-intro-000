d37969a00c084f1f050325a5cb9a805cf3d52449
Evan K Pattison <epattison@gmail.com>
2020-03-04 20:16:43 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,2 @@
 # Define the variable board below.
-board = board_array
-board_array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = board_array.new(" ", " ", " ", " ", " ", " ", " ", " ", " ")
